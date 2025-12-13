61f4c287423e216df4df4d1603554aa05b11ff2f
placid614 <willis.parker@olsohio.org>
2025-12-13 03:21:30 +0000

    ACTUALLY FIXES PASSWORD HASHING BEING STORED AS PLAINTEXT

--- blog/forms.py
+++ blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import Post
 from .models import Comment
+from django.contrib.auth.forms import UserCreationForm
 class PostForm(forms.ModelForm):
 
     class Meta:
@@ -13,7 +14,10 @@ class CommentForm(forms.ModelForm):
         model = Comment
         fields = ('author', 'text')
 
-class SignUpForm(forms.ModelForm):
+
+class SignUpForm(UserCreationForm):
+    email = forms.CharField(max_length=254, required=True, widget=forms.EmailInput())
+
     class Meta:
         model = User
-        fields = ('username', 'email', 'password')
+        fields = ('username', 'email', 'password1', 'password2')
